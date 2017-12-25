f73ff0fe571dc6eccdcb5abcd80b27cf1731727f
k0siara <patryk.kosieradzki@gmail.com>
2017-12-25 18:22:49 +0100

    moved brackets

--- Assets/Scripts/Pin.cs
+++ Assets/Scripts/Pin.cs
@@ -5,40 +5,31 @@ using UnityEngine;
 public class Pin : MonoBehaviour {
 
     public Rigidbody2D rb;
-
     public float speed;
+
     private bool isPinned;
 
     private void Start () {
         isPinned = false;
-        
     }
 
     private void Update () {
-        if (!isPinned)
-        {
+        if (!isPinned) {
             rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
         }
 	}
 
     private void OnTriggerEnter2D (Collider2D col) {
-        if (col.tag == "Rotator" && !FindObjectOfType<GameManager>().IsGameEnd())
-        {
+        if (col.tag == "Rotator" && !FindObjectOfType<GameManager>().IsGameEnd()) {
             transform.SetParent(col.transform);
             Score.points += 1;
             isPinned = true;
-        }
-        else if (col.tag == "Rotator" && FindObjectOfType<GameManager>().IsGameEnd())
-        {
+        } else if (col.tag == "Rotator" && FindObjectOfType<GameManager>().IsGameEnd()) {
             transform.SetParent(col.transform);
             isPinned = true;
-        }
-        else if (col.tag == "Pin")
-        {
-            if (col.transform.parent != null)
-            {
-                foreach (Transform child in col.transform.parent)
-                {
+        } else if (col.tag == "Pin") {
+            if (col.transform.parent != null) {
+                foreach (Transform child in col.transform.parent) {
                     child.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
                     transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
 
@@ -49,7 +40,6 @@ public class Pin : MonoBehaviour {
                 FindObjectOfType<GameManager>().EndGame();
                 FindObjectOfType<Spawner>().SetTriger(false);
             }
-
         }
     }
 }
