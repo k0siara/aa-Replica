569b54f57ffcc1a415ed4ecb26a1bf4e2c6a32d1
k0siara <patryk.kosieradzki@gmail.com>
2017-12-26 11:57:55 +0100

    added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject pauseMenuUI;
+
+    private void Update() {
+        
+    }
+
+    public void onPlayButtonTouch() {
+        pauseMenuUI.active = false;
+    }
+
+    public void onQuitButtonTouch() {
+        Application.Quit();
+        Debug.Log("QUIT !");
+    }
+}
